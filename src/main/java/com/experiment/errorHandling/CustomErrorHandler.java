bfabb48937f64d74ac6f605b0c83c132761cd97b
sagarika padhy <sagarika.padhy478@gmail.com>
2019-10-23 00:31:57 +1100

    fixed bug. rebuild jar

--- src/main/java/com/experiment/errorHandling/CustomErrorHandler.java
+++ src/main/java/com/experiment/errorHandling/CustomErrorHandler.java
@@ -3,14 +3,14 @@ package com.experiment.errorHandling;
 import com.experiment.application.ConsumeRestApiApplication;
 import com.experiment.model.APIResponse;
 
+/**
+ * Can be used to write custom error messages as this is extending RuntimeException
+ * like for NullPointerException, if(errorMessage instanceOf NullPointerException)
+ */
 public class CustomErrorHandler extends RuntimeException {
 
     public CustomErrorHandler(String errorMessage) {
         super(errorMessage);
-        ConsumeRestApiApplication consumeRestApiApplication =new ConsumeRestApiApplication();
-        APIResponse apiResponse =new APIResponse();
-        apiResponse.setErrorMessage(errorMessage);
-       // consumeRestApiApplication.sendEmailWithAttachments("",new JSONObject((Map) apiResponse);
 
     }
 
